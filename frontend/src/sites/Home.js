46d122b821c37657d1c05d2cc4e6dfe951b54f7c
Vojtěch Petrásek <vojtech.petrasek@comap-control.com>
2023-02-22 16:02:12 +0100

    remove grafana iframe

--- frontend/src/sites/Home.js
+++ frontend/src/sites/Home.js
@@ -10,12 +10,6 @@ class Home extends Component {
                 <NavBar />
                 <div className="container-fluid row">
                     <h1>Body</h1>
-                    <iframe
-                        src="https://grafana.uu.vojtechpetrasek.com/d-solo/xRpNuj1Vk/uu-team-8?orgId=1&from=1677068325673&to=1677071925673&theme=dark&panelId=2"
-                        width="450"
-                        height="200"
-                        frameBorder="0"
-                    ></iframe>
                 </div>
                 <Footer />
             </>
