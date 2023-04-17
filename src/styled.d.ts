69ce42696832b18e66824c6109fc24b1321a5bdc
Vojtěch Novotný <vojta.nvtn@gmail.com>
2023-04-17 22:06:11 +0200

    Smazání develop z masteru

--- src/styled.d.ts
+++
@@ -1,8 +0,0 @@
-import "@emotion/react";
-import { Theme } from "./src/style-variables";
-
-type CustomTheme = typeof Theme;
-
-declare module "@emotion/react" {
-    export interface Theme extends CustomTheme { }
-}
