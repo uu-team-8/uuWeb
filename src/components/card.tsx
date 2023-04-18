975c80629861eff5d1e4a317504805bd39ad977b
Vojtěch Novotný <vojta.nvtn@gmail.com>
2023-04-18 11:02:47 +0200

    Dashboard

---
+++ src/components/card.tsx
@@ -0,0 +1,46 @@
+import type { FC } from "react";
+import styled from "@emotion/styled";
+
+interface CardProps {
+    title: string
+}
+
+const Card: FC<CardProps> = ({ title }) => {
+    return (
+        <Wrapper>
+            <Title>{title}</Title>
+            <Divider />
+        </Wrapper>
+    )
+}
+
+const Wrapper = styled("div")`
+    min-width: 479px;
+    min-height: 378px;
+    position: relative;
+    padding: 16px;
+    border-radius: 4px;
+    background-color: ${p => p.theme.gray.gray800};
+    box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px rgba(0, 0, 0, 0.14), 0px 1px 3px rgba(0, 0, 0, 0.12);
+`
+
+const Title = styled("h2")`
+    color: ${p => p.theme.UI.white};
+    font-family: "Helvetica";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 32px;
+    letter-spacing: -0.25px;
+`
+
+const Divider = styled("div")`
+    width: 100%;
+    height: 1px;
+    background-color: ${p => p.theme.gray.gray700};
+    position: absolute;
+    top: 64px;
+    left: 0px;
+`
+
+export default Card;
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,20 +1,15 @@
 import type { FC } from "react";
 import styled from "@emotion/styled";
 
+import Card from "../components/card";
+
 const Home: FC = () => {
     return (
         <Wrapper>
             <Title>Dashboard</Title>
             <CardContainer>
-                <Card>
-                    <CardTitle>Teplota</CardTitle>
-                    <Divider />
-                </Card>
-
-                <Card>
-                    <CardTitle>Vlhkost</CardTitle>
-                    <Divider />
-                </Card>
+                <Card title="Teplota" />
+                <Card title="Vlhkost" />
             </CardContainer>
         </Wrapper>
     )
@@ -43,35 +38,6 @@ const CardContainer = styled("div")`
     flex-direction: row;
     justify-content: space-around;
     margin-top: 160px;
-`
-
-const Card = styled("div")`
-    min-width: 479px;
-    min-height: 378px;
-    position: relative;
-    padding: 16px;
-    border-radius: 4px;
-    background-color: ${p => p.theme.gray.gray800};
-    box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px rgba(0, 0, 0, 0.14), 0px 1px 3px rgba(0, 0, 0, 0.12);
-`
-
-const CardTitle = styled("h2")`
-    color: ${p => p.theme.UI.white};
-    font-family: "Helvetica";
-    font-style: normal;
-    font-weight: 700;
-    font-size: 24px;
-    line-height: 32px;
-    letter-spacing: -0.25px;
-`
-
-const Divider = styled("div")`
-    width: 100%;
-    height: 1px;
-    background-color: ${p => p.theme.gray.gray700};
-    position: absolute;
-    top: 64px;
-    left: 0px;
 `
 
 export default Home;
