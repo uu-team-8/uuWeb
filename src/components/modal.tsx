666b22bfdbaa8d3dcb8bc05ce0f486744f6e44ed
ondrapetrzilka <ondra.petrzilka@gmail.com>
2023-05-23 00:46:45 +0200

    Modal redesign

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -50,7 +50,7 @@ const ModalContainer = styled.div`
     position: absolute;
     width: 100%;
     height: 100%;
-    padding: 100% 0%;
+    padding: 100% 0;
     background-color: rgba(0, 0, 0, .8);
 `
   
@@ -58,9 +58,8 @@ const ModalContainer = styled.div`
     position: relative;
     min-width: 350px;
     min-height: 174px;
-    padding: 20px 40px 20px 30px;
-    background: #E3E3E3;
-    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.08), 0px 4px 20px rgba(0, 0, 0, 0.12);
+    padding: 20px 40px;
+    background: white;
     border-radius: 14px;
     @media (max-width: 420px) {
         min-width: 300px;
@@ -72,21 +71,21 @@ const ModalContainer = styled.div`
   
 const ModalTitle = styled.h1`
     font-style: normal;
-    font-weight: 300;
     font-size: 24px;
     line-height: 24px;
     text-align: center;
-    color: #0F0E12;
+    color: black;
+    font-weight: bold;
 `
 
 const ModalDescription = styled.h2`
     font-style: normal;
-    font-weight: 300;
+    font-weight: normal;
     font-size: 18px;
     line-height: 18px;
     text-align: center;
-    color: #7F7F7F;
-    margin: 10px 0px 27px 0px;
+    color: black;
+    margin: 10px 0 27px 0;
 `
 
 const ButtonContainer = styled.div`
@@ -95,31 +94,43 @@ const ButtonContainer = styled.div`
 `
 
 const CloseButton = styled.button`
-    display: flex;
-    text-align: center;
-    justify-content: center;
-    padding: 15px;
-    margin-left: 50px;
-    width: 142px;
-    min-height: 42px;
-    background: #EB5545;
-    border-radius: 8px;
-    font-weight: 300;
-    font-size: 16px;
-    line-height: 18px;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  padding: 15px;
+  margin-left: 50px;
+  width: 142px;
+  min-height: 42px;
+  background: white;
+  border-radius: 14px;
+  font-weight: 300;
+  font-size: 16px;
+  line-height: 18px;
+  border: 1px solid darkred;
+  color: darkred;
 
-    &:hover {
-        opacity: 0.8;
-        cursor: pointer;
-    }
+  &:hover {
+    background: darkred;
+    border: 1px solid darkred;
+    cursor: pointer;
+    color: white;
+  }
 
-    &:active {
-        opacity: 0.6;
-    }
+  &:active {
+    opacity: 0.8;
+  }
 `
 
 const CancelButton = styled(CloseButton)`
-    background: #FFFFFF; 
-    color: #0F0E12;
-    margin: 0;
+  background: #FFFFFF;
+  color: black;
+  margin: 0;
+  border: 1px solid black;
+
+  &:hover {
+    background: black;
+    border: 1px solid black;
+    color: white;
+    cursor: pointer;
+  }
 `
